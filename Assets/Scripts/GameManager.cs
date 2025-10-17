d633efd9423f47316bb532add116336c0a2dc7e4
Artemiz <artemiz.onion@gmail.com>
2025-10-17 14:39:44 -0300

    chore(proj): adiciona a estrutura base do projeto

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int pontos;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        // Debug.Log(Application.dataPath + "/Scripts/save.json");
+        //Para carregar automaticamente ao iniciar
+        GameObject player = GameObject.FindWithTag("Player");
+        CarregarJogo(player);
+    }
+
+    public void AdicionarPontos(int valor)
+    {
+        pontos += valor;
+    }
+
+    public void SalvarJogo(Vector3 playerPos)
+    {
+        SaveSystem.SalvarJogo(playerPos, pontos);
+    }
+
+    public void CarregarJogo(GameObject player)
+    {
+        SaveData data = SaveSystem.CarregarJogo();
+        if (data != null)
+        {
+            player.transform.position = new Vector3(data.playerX, data.playerY, data.playerZ);
+            pontos = data.pontos;
+        }
+    }
+}
